02e267702d4ab75330b64a1dc6f833e0aeb13121
davidbt07 <david.ballesteros@udea.edu.co>
2021-03-15 18:06:24 -0500

    Starting Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import 'hammerjs';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
@@ -27,7 +31,10 @@ import { DetailsComponent } from './details/details.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatListModule,
+    MatGridListModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+  items = [3,2]
   constructor() { }
 
   ngOnInit(): void {
